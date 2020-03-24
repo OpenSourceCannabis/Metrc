9dab3388a80902cbb2a4efd99f52e7608d8b2d5a
Dan de Havilland <dan@dandehavilland.com>
2020-03-24 08:53:19 +0100

    Adding POST /harvests/v1/packages/create(/testing)

--- lib/Metrc/client.rb
+++ lib/Metrc/client.rb
@@ -154,6 +154,10 @@ module Metrc
       api_post("/plantbatches/v1/destroy?licenseNumber=#{license_number}", body: resources.to_json)
     end
 
+    def create_harvest_package(license_number, resources, for_testing = false)
+      api_post("/harvests/v1/create/packages#{for_testing ? "/testing" : ''}?licenseNumber=#{license_number}", body: resources.to_json)
+    end
+
     def create_package(license_number, resources, for_testing = false)
       api_post("/packages/v1/create#{for_testing ? "/testing" : ''}?licenseNumber=#{license_number}", body: resources.to_json)
     end
